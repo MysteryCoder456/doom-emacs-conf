dc577b6dabd280443eff99d16300627cf77f1ae2
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-03-29 13:36:33 +0400

    enabled soft line wrapping

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(menu-bar-mode nil)
  '(package-selected-packages '(treemacs-evil treemacs))
@@ -14,4 +15,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight normal :height 140 :width normal)))))
