251f22be3ff7527a66304b4042d8af53ceff5372
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-04-14 13:37:51 +0400

    lsp-jedi config

--- config.el
+++ config.el
@@ -76,3 +76,10 @@
 ;; they are implemented.
 
 (setq treemacs-width 30)
+
+(use-package lsp-jedi
+  :ensure t
+  :config
+  (with-eval-after-load "lsp-mode"
+    (add-to-list 'lsp-disabled-clients 'pyls)
+    (add-to-list 'lsp-enabled-clients 'jedi)))
