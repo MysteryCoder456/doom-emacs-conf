d6943e79b4ace0a6effbe58dbbd18a3d8defa431
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-04-27 18:35:20 +0400

    added rust-analyzer to thing

--- config.el
+++ config.el
@@ -88,7 +88,7 @@
   :config
   (with-eval-after-load "lsp-mode"
     (add-to-list 'lsp-disabled-clients 'pyls)
-    (add-to-list 'lsp-enabled-clients 'pyright)))
+    (add-to-list 'lsp-enabled-clients 'pyright 'rust-analyzer)))
 
 (use-package elpy
   :ensure t
