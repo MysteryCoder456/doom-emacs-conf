98745ac34866139325947d12e17fcc239f10122c
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-07-07 15:14:04 +0400

    added doom-xcode theme

--- config.el
+++ config.el
@@ -75,6 +75,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(load-theme 'doom-xcode)
 (setq treemacs-width 30)
 (blink-cursor-mode)
 (centaur-tabs-mode)
--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" default))
  '(warning-suppress-types '((initialization))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
